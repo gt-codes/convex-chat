0876e80efe12eaac9c54a3e8d486805cfd50028b
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-08-26 00:08:29 -0500

    :it:works:

--- convex/getRooms.ts
+++ convex/getRooms.ts
@@ -1,22 +1,7 @@
 import { GenericId } from 'convex/values';
 import { query } from './_generated/server';
 
-export default query(async ({ db }, userId: GenericId<'users'> | null | undefined) => {
-	if (!userId) return null;
-	console.log('finding rooms');
-
+export default query(async ({ db }, userId: GenericId<'users'>) => {
 	const rooms = await db.table('rooms').collect();
-
-	const filteredRooms = rooms.forEach((el) => {
-		if (el.members.some((member) => member.id === userId.id)) {
-			console.log({
-				roomName: el.name,
-				userId: userId.id,
-				members: el.members.map((el) => el.id),
-			});
-		}
-		return el.members.some((member) => member.id === userId.id);
-	});
-	console.log({ filteredRooms });
 	return rooms.filter((el) => el.members.some((member) => member.id === userId.id));
 });
