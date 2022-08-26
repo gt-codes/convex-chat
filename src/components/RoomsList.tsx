6c5219f8da2a78ebba0f05dba3284ce244fcba4d
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-08-25 21:42:17 -0500

    rooms list opens to dynamic route

--- src/components/RoomsList.tsx
+++ src/components/RoomsList.tsx
@@ -1,6 +1,7 @@
-import { GenericId } from "convex/values";
-import { useQuery } from "../../convex/_generated/react";
-import { format } from "date-fns";
+import { GenericId } from 'convex/values';
+import { useQuery } from '../../convex/_generated/react';
+import { format } from 'date-fns';
+import Link from 'next/link';
 
 export default function RoomsList({ userId }: { userId: GenericId<'users'> }) {
 	const rooms = useQuery('getRooms', userId);
@@ -8,14 +9,16 @@ export default function RoomsList({ userId }: { userId: GenericId<'users'> }) {
 	return (
 		<ul className='bg-white w-full mt-3 p-3 rounded-lg space-y-3 divide-y divide-gray-50'>
 			{rooms?.map((room) => (
-				<li key={room._id.id} className='text-gray-800'>
-					<div className=''>
-						<p className='text-gray-800 font-medium'>{room.name}</p>
-						<p className='text-gray-400 text-xs font-light'>
-							{format(room._creationTime, 'MMM do, yyyy h:mm a')}
-						</p>
+				<Link href={`/r/${room._id.id}`} key={room._id.id}>
+					<div className='hover:bg-gray-50 cursor-pointer py-1 px-2 rounded-md text-gray-800'>
+						<div className=''>
+							<p className='text-gray-800 font-medium'>{room.name}</p>
+							<p className='text-gray-400 text-xs font-Linkght'>
+								{format(room._creationTime, 'MMM do, yyyy h:mm a')}
+							</p>
+						</div>
 					</div>
-				</li>
+				</Link>
 			))}
 		</ul>
 	);
